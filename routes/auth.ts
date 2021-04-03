38b2ca874e16fda2798d908a662d8afa54558f23
Johan Altamar <johan.altamar.dev@gmail.com>
2021-04-03 01:05:26 -0500

    add devices token route

--- routes/auth.ts
+++ routes/auth.ts
@@ -1,8 +1,26 @@
-import { Router } from 'express';
-import { login } from '../controllers/auth';
+import { Router } from "express";
+import { login, newDeviceToken } from "../controllers/auth";
+import { hasRole } from "../middlewares/roles";
+import { validateSessionToken } from "../middlewares/tokens";
 
 const router = Router();
 
-router.post("/login", login)
+/**
+ * USER LOGIN ROUTE
+ * Public route
+ * email required and valid, password required
+ */
+router.post("/login", login);
+
+/**
+ * CREATE A NEW GROUP DEVICES TOKEN ROUTE
+ * Protected route
+ * ADMIN ROLE required
+ */
+router.post(
+  "/devices-token",
+  [validateSessionToken, hasRole("ADMIN_ROLE")],
+  newDeviceToken
+);
 
 export default router;
